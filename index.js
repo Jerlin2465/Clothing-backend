648255f776b61ab73adc9759ae9f23ab2248178a
Jerlin2465 <jerlinjack246@gmail.com>
2026-05-05 14:59:15 +0530

    FEAT:fix router

--- index.js
+++ index.js
@@ -28,6 +28,7 @@ app.use(
   cors({
     origin: [
       "http://localhost:5173",
+      "http://localhost:5001",
       "https://clothing-e-com-alpha.vercel.app",
       "https://clothing-e-com-v6ww.onrender.com",
     ],
