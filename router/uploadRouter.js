8e9e4b7afee02516f215a2eedf2b8e5abaabb681
Jerlin2465 <jerlinjack246@gmail.com>
2026-05-05 13:10:11 +0530

    FEAT:updated

--- router/uploadRouter.js
+++ router/uploadRouter.js
@@ -15,7 +15,7 @@ const authmiddleware = require("../middleware/authmiddleware");
 const router = express.Router();
 
 router.post("/upload", upload.array("image", 4), createProduct);
-router.get("/get-product", getProduct);
+router.get("/", getProduct);
 router.get("/get-product/:id", getSingleProduct);
 router.put("/update-product/:id", upload.array("image", 4), updateProduct);
 router.delete("/delete-product/:id", deleteProduct);
