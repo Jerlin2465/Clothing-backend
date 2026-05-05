6ebd17ccaf3df3793f4327694c031ba10917fd64
Jerlin2465 <jerlinjack246@gmail.com>
2026-05-05 15:29:44 +0530

    FEAT:updated

--- index.js
+++ index.js
@@ -26,13 +26,27 @@ app.use(cookieParser());
 
 app.use(
   cors({
-    origin: [
-      "http://localhost:5173",
-      "http://localhost:5001",
-      "http://localhost:5000",
-      "https://clothing-e-com-alpha.vercel.app",
-      "https://clothing-e-com-v6ww.onrender.com",
-    ],
+    origin: function (origin, callback) {
+      const allowedOrigins = [
+        "http://localhost:5173",
+        "http://localhost:5000",
+        "http://localhost:5001",
+        "https://clothing-e-com-alpha.vercel.app",
+        "https://clothing-e-com-v6ww.onrender.com",
+      ];
+
+      if (!origin) return callback(null, true);
+
+      if (
+        allowedOrigins.includes(origin) ||
+        /^http:\/\/192\.168\.\d+\.\d+(:\d+)?$/.test(origin) ||
+        /^http:\/\/10\.\d+\.\d+\.\d+(:\d+)?$/.test(origin)
+      ) {
+        return callback(null, true);
+      }
+
+      return callback(new Error("Not allowed by CORS"));
+    },
     credentials: true,
   }),
 );
