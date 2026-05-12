2dc340a88d63516e1d14e30583dc90a1d0209fc5
Jerlin2465 <jerlinjack246@gmail.com>
2026-05-12 10:27:45 +0530

    FEAT:URL_update

--- index.js
+++ index.js
@@ -31,7 +31,7 @@ app.use(
         "http://localhost:5173",
         "http://localhost:5000",
         "http://localhost:5001",
-        "https://blackpearl-kappa.vercel.app/",
+        "blackpearl-kappa.vercel.app",
         "https://clothing-e-com-v6ww.onrender.com",
       ];
 
